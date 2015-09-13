f6328969dcb24e2b2bde17b16b43acc27221a092
Mark Chavez <markchav3z@gmail.com>
2015-09-13 11:02:50 +0800

    gitignore is now reloaded

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-key = Rails.application.secrets.facebook_key
-secret = Rails.application.secrets.facebook_secret
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, key, secret
-end
