30f61de83be0b4c7ddfa54634e98a909948f7ef8
Mark Chavez <markchav3z@gmail.com>
2015-09-13 19:42:23 +0800

    Test omniauth creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :provider, :uid, presence: true
+
   def self.from_omniauth(auth_hash)
     find_or_create_by(provider: auth_hash.provider, uid: auth_hash.uid) do |user|
       user.name = auth_hash.info.name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,9 @@
 require "rails_helper"
 
 describe User do
-  let!(:facebook_hash) do
-    eval(File.read("spec/fixtures/facebook_hash.yml"))
-  end
+  let!(:facebook_hash) { eval(File.read("spec/fixtures/facebook_hash.yml")) }
 
-  describe ".from_omniauth" do
+  describe "from facebook hash" do
     it "has a provider" do
       expect(facebook_hash[:provider]).not_to be_empty
     end
@@ -20,6 +18,23 @@ describe User do
     
     it "has an image" do
       expect(facebook_hash[:info][:image]).not_to be_empty
+    end
+  end
+
+  describe ".from_omniauth" do
+    context "provider and uid not on database" do
+      it "creates a new record" do
+        hash_info = double("info", name: "sample", image: "image")
+        hash_auth = double("auth", provider: "12345", uid: "12345", info: hash_info)
+
+        expect { 
+          User.from_omniauth(hash_auth)
+        }.to change{ User.all.count }.by(1)
+      end
+    end
+
+    context "provider and uid on database" do
+
     end
   end
 end
